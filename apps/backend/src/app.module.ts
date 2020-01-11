911947479bbe342c062da2b00e51400b30472110
Askhat Bikmetov <lolborgini@gmail.com>
2020-01-11 22:20:38 +0300

    Update app

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,5 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { BucketModule } from "./bucket.module";
+import { FileModule } from './file.module';
 
 @Module({
+  imports: [BucketModule, FileModule],
 })
-export class AppModule {}
+export class AppModule { }
