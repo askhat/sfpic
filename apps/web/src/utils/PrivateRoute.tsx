2b7e3ac8b22c251264ed2fd25da70cc5bad50063
Askhat Bikmetov <lolborgini@gmail.com>
2020-01-14 14:21:36 +0300

    Fix private route being too protective

--- apps/web/src/utils/PrivateRoute.tsx
+++ apps/web/src/utils/PrivateRoute.tsx
@@ -1,14 +1,12 @@
 import React, { useContext } from "react";
 import { Route, RouteProps, Redirect } from "react-router-dom";
 import { User } from "~context";
+import { Spinner } from "~components";
 
 export function PrivateRoute({ children, ...props }: RouteProps) {
   let user = useContext(User);
 
   return (
-    <Route
-      {...props}
-      render={() => (user.isAuth ? children : <Redirect to="/welcome" />)}
-    />
+    <Route {...props} render={() => (user.isAuth ? children : <Spinner />)} />
   );
 }
