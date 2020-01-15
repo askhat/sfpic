14c37760a859442c793230c2ecb84c9acafe9cd7
Askhat Bikmetov <lolborgini@gmail.com>
2020-01-15 04:02:35 +0300

    Update interfaces

---
+++ apps/backend/src/file.interface.ts
@@ -0,0 +1,10 @@
+export interface File {
+  /** Name of a file in the storage */
+  _id: string;
+  /** Original file name provided by a user */
+  name: string;
+  /** Mime Type */
+  type: string;
+  /** Size in bytes */
+  size: number;
+}
